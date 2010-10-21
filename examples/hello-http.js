4136511704093f4c293bbc97cce48c1b964e6b27
Elijah Insua <tmpvar@gmail.com>
2010-10-20 22:59:00 -0400

    converted the hello-http example to use the 'human readable' route setup format

--- examples/hello-http.js
+++ examples/hello-http.js
@@ -5,43 +5,37 @@ var conductor = require("../lib/conductor").conductor,
     flow      = conductor(),
 
     // Create a semi-static node that returns a port number
-    port      = flow.node(function() { return 8000 }),
+    port      = flow.node(function() { return 8000 }, "port"),
 
     // Create a node that spawns an http server on a specified port, and
     // emits a request/response
-    listener  = flow.node(function(port, requestFn, responseFn) {
+    listener  = flow.node(function(port, request, response) {
                   http.createServer(function(req, res) {
-                    if (requestFn) {
-                      requestFn(req);
+                    if (request) {
+                      request(req);
                     }
-                    if (responseFn) {
-                      responseFn(res);
+                    if (response) {
+                      response(res);
                     }
                   }).listen(port);
-                }),
+                }, "listener"),
 
     // Create a node to handle the incoming request (routing/etc)
     handler   = flow.node(function(request) { 
                   return "hello world!\n"
-                }),
+                }, "handler"),
 
     // Create a node that will respond to the request
     responder = flow.node(function(body, response) {
                   response.writeHead(200, {'Content-Type': 'text/plain'});
                   response.end(body);
-                });
+                }, "responder");
 
-// Route the output of the `port` to the first input of the `listener`
-port.output(listener.input(0));
-
-// Route the request to the `handler`'s first input port
-listener.args.request(handler.request());
-
-// Route the output of the `handler` to the `responder`'s first input
-handler.output(responder.body())
-
-// Route the `response` from the listener to the second input of the `responder` node
-listener.input(2, responder.input(1));
+// Setup routes, read left to right.
+port.output(listener.args.port());
+listener.args.request(handler.args.request());
+handler.output(responder.args.body())
+listener.args.response(responder.args.response());
 
 // We now have a flow that looks something like:
 //
